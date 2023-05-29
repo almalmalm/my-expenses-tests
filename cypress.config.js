b21415bbf8391616bc04115577d48ce3706f1fc9
almalmalm <almostkalinin@gmail.com>
2023-05-29 23:39:21 +0400

    ADD: QASE integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,25 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-qase-reporter',
+  reporterOptions: {
+    // reporterEnabled: 'cypress-mochawesome-reporter, cypress-qase-reporter',
+    // cypressMochawesomeReporterReporterOptions: {
+    //   charts: true,
+    // },
+    // cypressQaseReporterReporterOptions: {
+    apiToken: process.env.QASE_API_TOKEN,
+    projectCode: 'ME',
+    // logging: true,
+    // basePath: 'https://api.qase.io/v1',
+    // screenshotFolder: 'screenshots',
+    // sendScreenshot: true,
+    runComplete: true,
+    // environmentId: 1,
+    // rootSuiteTitle: 'Cypress tests',
+    // },
+  },
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
